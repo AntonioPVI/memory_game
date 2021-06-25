86814c8b4a3002138256928069d1bed71760e802
AntonioPVI <antoniogarciamontes01@gmail.com>
2021-06-25 13:16:39 +0200

    cards options

---
+++ js/app.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+
+    //Card options
+    const cardArray = [
+        {
+            name: 'fries',
+            img:'img/fries.png'
+        },
+        {
+            name: 'fries',
+            img:'img/fries.png'
+        },
+        {
+            name: 'cheeseburger',
+            img:'img/cheeseburger.png'
+        },
+        {
+            name: 'cheeseburger',
+            img:'img/cheeseburger.png'
+        },
+        {
+            name: 'hotdog',
+            img:'img/hotdog.png'
+        },
+        {
+            name: 'hotdog',
+            img:'img/hotdog.png'
+        },
+        {
+            name: 'ice-cream',
+            img:'img/ice-cream.png'
+        },
+        {
+            name: 'ice-cream',
+            img:'img/ice-cream.png'
+        },
+        {
+            name: 'milkshake',
+            img:'img/milkshake.png'
+        },
+        {
+            name: 'milkshake',
+            img:'img/milkshake.png'
+        },
+        {
+            name: 'pizza',
+            img:'img/pizza.png'
+        },
+        {
+            name: 'pizza',
+            img:'img/pizza.png'
+        },
+    ]
+
+});
